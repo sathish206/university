546c65a938c60dda835229665fee431fba3406b0
sathish206 <dillisathish@yahoo.com>
2021-04-07 12:09:59 +0530

    task

---
+++ src/org/univ/College.java
@@ -0,0 +1,18 @@
+package org.univ;
+
+public class College extends University{
+public void ug() {System.out.println("B.E");
+	
+}
+public void pg() {System.out.println("M.Tech");
+	
+}
+public static void main(String[] args) {
+	University u=new University();
+	u.ug();
+	u.pg();
+	College c=new College();
+	c.ug();
+	c.pg();
+}
+}
---
+++ src/org/univ/University.java
@@ -0,0 +1,9 @@
+package org.univ;
+
+public class University {public void ug() {System.out.println("ug");
+	
+}
+public void pg() {System.out.println("pg");
+	
+}
+}
